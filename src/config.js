434b23f03ca90dc95217c6cb36628cfda85466d3
Ye Lin <yel106@ucsd.edu>
2018-05-31 11:35:25 -0700

    ?????

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG ={
-    apiKey: "AIzaSyAw8bha316J7zLwz-JN2GaUp3w8RioRxP0",
-    authDomain: "csehub-420.firebaseapp.com",
-    databaseURL: "https://csehub-420.firebaseio.com",
-    projectId: "csehub-420",
-    storageBucket: "csehub-420.appspot.com",
-    messagingSenderId: "719021972711"
-}
